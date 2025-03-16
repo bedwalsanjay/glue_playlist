049a1142b52b1aaa24ec746c359a9ff2231d9fd7
SanjayBedwal <sanjay.bedwal@gmail.com>
2025-03-16 18:49:33 +0530

    adding some notebook files and sample files for practice

---
+++ test2.py
@@ -0,0 +1,8 @@
+#write a function to get the sum of two numbers
+def add(a,b):
+    return a+b
+# Call the function
+print(add(3,4))
+
+
+# Open the Chat view from the Command Center Copilot menu or press Ctrl+Alt+I
