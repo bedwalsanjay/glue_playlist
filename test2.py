987ac330b525f9e4d28b3618434213eb1fdb93ff
Sanjay Bedwal <sanjay.10738538@ltimindtree.com>
2025-04-05 17:53:22 +0530

    restructured the folder hierarchy

--- test2.py
+++
@@ -1,8 +0,0 @@
-#write a function to get the sum of two numbers
-def add(a,b):
-    return a+b
-# Call the function
-print(add(3,4))
-
-
-# Open the Chat view from the Command Center Copilot menu or press Ctrl+Alt+I
